ebf6708d0fe7a51df52c113925db4385edb9e117
april Prospero Garciagit config --global user.email april.prospero@gmail.com <april.prospero@gmail.com>
2021-01-08 21:08:00 -0700

    read json

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,6 +1,17 @@
-// Use D3 fetch to read the JSON file
-// The data from the JSON file is arbitrarily named importedData as the argument
-d3.json("data/samples.json").then((importData) => {
 
-   
-)}
+function dashboard() {
+  // Use D3 to select the dropdown menu
+  var dropdownMenu = d3.select("#selDataset");
+
+// Use the D3 library to read in `samples.json`.
+d3.json("data/samples.json").then((importedData) => {
+  var data = importedData;
+  console.log(data);
+  data.names.forEach(d =>{
+      dropdownMenu.append("option").text(d).property("value");
+  });
+  optionChanged(data.names[0])
+});   
+};
+dashboard() 
+
